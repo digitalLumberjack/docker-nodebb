3caeda6a196e9090ff588038bbaef7aede5e29bb
digitalLumberjack <digitallumberjack@gmail.com>
2016-07-07 23:29:56 +0200

    added automatic module install at startup

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,13 @@
 FROM node:4.4
 ARG NODEBB_VERSION=v1.0.4-auto.11
 
-RUN apt-get update && apt-get install -y gettext-base
-
 ENV NODE_ENV=production \
     daemon=false \
     silent=false
 ENV NODEBB_URL ''
 ENV NODEBB_PORT ''
 ENV NODEBB_SECRET ''
+ENV NODEBB_PLUGINLIST ''
 ENV MONGO_HOST 'mongo'
 ENV MONGO_PORT '27017'
 ENV MONGO_USERNAME ''
@@ -30,6 +29,8 @@ ENV AWS_SECRET_ACCESS_KEY ''
 ENV S3_UPLOADS_BUCKET ''
 ENV S3_UPLOADS_HOST ''
 
+RUN apt-get update && apt-get install -y gettext-base ssmtp
+
 WORKDIR /opt/nodebb
 ADD https://github.com/NodeBB/NodeBB/archive/${NODEBB_VERSION}.tar.gz /opt/nodebb.tar.gz
 RUN tar xvzf /opt/nodebb.tar.gz -C /opt/nodebb --strip 1
@@ -38,6 +39,5 @@ ADD conf/config.json.template /opt/nodebb/config.json.template
 ADD conf/ssmtp.conf.template /etc/ssmtp/ssmtp.conf.template
 
 RUN npm install --production
-RUN npm install nodebb-plugin-s3-uploads-updated@0.3.10
 
 CMD /usr/local/bin/start.sh
--- start.sh
+++ start.sh
@@ -2,15 +2,18 @@
 
 if [ ! -f /opt/nodebb/.stamp_installed ];then
   echo "creating ssmtp configuration"
-  envsubst < /etc/ssmtp/ssmtp.conf.template > /etc/ssmtp/ssmtp.conf 
-  [[ "$?" != "0" ]] && echo "Unable to create ssmtp configuration" && exit 1
+  envsubst < /etc/ssmtp/ssmtp.conf.template > /etc/ssmtp/ssmtp.conf || (echo "Unable to create ssmtp configuration" && exit 1)
   echo "creating nodebb config.json"
-  envsubst < /opt/nodebb/config.json.template > /opt/nodebb/config.json 
-  [[ "$?" != "0" ]] && echo "Unable to create nodebb config.json" && exit 1
+  envsubst < /opt/nodebb/config.json.template > /opt/nodebb/config.json || (echo "Unable to create nodebb config.json" && exit 1)
+  for module in ${NODEBB_PLUGINLIST};do
+    echo "installing $module"
+    npm install "$module" || (echo "Unable to install $module" && exit 1)
+    moduleToActivate="${moduleToActivate}, \"`echo $module | cut -f1 -d'@'`\""
+  done
+  IFS=$OIFS
+  node app.js --setup "{\"admin:username\":\"${ADMIN_USERNAME}\",\"admin:password\":\"${ADMIN_PASSWORD}\",\"admin:password:confirm\":\"${ADMIN_PASSWORD}\",\"admin:email\":\"${ADMIN_EMAIL}\"}" --defaultPlugins "[\"nodebb-plugin-composer-default\",\"nodebb-plugin-markdown\",\"nodebb-plugin-mentions\",\"nodebb-widget-essentials\",\"nodebb-rewards-essentials\",\"nodebb-plugin-soundpack-default\",\"nodebb-plugin-emoji-extended\",\"nodebb-plugin-emoji-one\",\"nodebb-plugin-dbsearch\",\"nodebb-plugin-spam-be-gone\"${moduleToActivate}]" || (echo "Unable to install nodebb" && exit 1) 
   touch /opt/nodebb/.stamp_installed
 fi
 
 cd /opt/nodebb
-node app.js --setup "{\"admin:username\":\"${ADMIN_USERNAME}\",\"admin:password\":\"${ADMIN_PASSWORD}\",\"admin:password:confirm\":\"${ADMIN_PASSWORD}\",\"admin:email\":\"${ADMIN_EMAIL}\"}" --defaultPlugins '["nodebb-plugin-composer-default","nodebb-plugin-markdown","nodebb-plugin-mentions","nodebb-widget-essentials","nodebb-rewards-essentials","nodebb-plugin-soundpack-default","nodebb-plugin-emoji-extended","nodebb-plugin-emoji-one","nodebb-plugin-s3-uploads-updated"]'
-[[ "$?" != "0" ]] && echo "Unable to install nodebb" && exit 1
 ./nodebb start
